50517967114e3c2198beeb092f6ed396faef4a48
IGE.FREDERICK.OLUFEMI.1505449017 <frederick.o.ige.ctr@mail.mil>
2017-12-06 13:11:32 -0500

    created GamerService

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var myGamerApp = angular.module("myGamerApp", []);
 //
 // }]);
 
-myGamerApp.controller("GamerController", ['$scope', '$timeout', function($scope, $timeout){
+myGamerApp.controller("GamerController", ['$scope', '$timeout','GamerService', function($scope, $timeout,GamerService){
 
 //how to remove from array
 $scope.removeGamer = function(gamer){
@@ -60,37 +60,6 @@ $scope.newgamer.bounty = "";
 
 
 
-$scope.gamers = [
-  {
-    name:"Victor",
-    level: "Pro",
-    bounty: 10000000000,
-    available: true,
-    thumb: "img/mage.png"
-
-  },
-  {
-    name:"Chris",
-    level: "Noob",
-    bounty: 2,
-    available: true,
-    thumb: "img/mush.png"
-  },
-  {
-    name:"Nhu",
-    level: "Expert",
-    bounty: 30000,
-    available: true,
-    thumb:"img/thief.png"
-  },
-  {
-    name:"Henry",
-    level: "Noob",
-    bounty: 1,
-    available: false,
-    thumb:"img/pig.png"
-  }
-
-];
+$scope.gamers = GamerService.getGamersList();
 
 }]);
