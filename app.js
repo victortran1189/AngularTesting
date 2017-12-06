c2dcfb50aa00c2c615344fb86f253b531bae50d8
Tran <1541469393@mil>
2017-12-06 14:58:21 -0500

    Services Done

--- app.js
+++ app.js
@@ -15,82 +15,43 @@ var myGamerApp = angular.module("myGamerApp", []);
 //
 // }]);
 
-myGamerApp.controller("GamerController", ['$scope', '$timeout', function($scope, $timeout){
+myGamerApp.controller("GamerController", ['$scope', '$timeout', 'GamerService', function($scope, $timeout, GamerService) {
 
-//how to remove from array
-$scope.removeGamer = function(gamer){
-  var removeGamer = $scope.gamers.indexOf(gamer);
-  $scope.gamers.splice(removeGamer, 1)
-};
+  //how to remove from array
+  $scope.removeGamer = function(gamer) {
+    var removeGamer = $scope.gamers.indexOf(gamer);
+    $scope.gamers.splice(removeGamer, 1)
+  };
 
-//how to add to array
-$scope.addGamer = function(){
+  //how to add to array
+  $scope.addGamer = function() {
 
-   var newGamerName = $scope.newgamer.name;
-   var matches = true;
-
-
-   angular.forEach($scope.gamers, function(gamers) {
-          if (newGamerName.toUpperCase() == gamers.name.toUpperCase()) {
-              matches = false;
-              $scope.message = 'This is a duplicate!';
-          }
-          $timeout(function() {
-              $scope.message = "";
-                }, 3000);
-          });
-
-      if (matches != false) {
-          $scope.gamers.push({
-            name: $scope.newgamer.name,
-            level: $scope.newgamer.level,
-            bounty: parseInt($scope.newgamer.bounty),
-          });
-          $scope.message = '';
+    var newGamerName = $scope.newgamer.name;
+    var matches = true;
 
 
-$scope.newgamer.name = "";
-$scope.newgamer.level = "";
-$scope.newgamer.bounty = "";
+    angular.forEach($scope.gamers, function(gamers) {
+      if (newGamerName.toUpperCase() == gamers.name.toUpperCase()) {
+        matches = false;
+        $scope.message = 'This is a duplicate!';
+      }
+      $timeout(function() {
+        $scope.message = "";
+      }, 3000);
+    });
 
-  }
-}
+    if (matches != false) {
+      $scope.gamers.push(GamerService.createNewGamerObject($scope.newgamer));
 
 
+      $scope.newgamer.name = "";
+      $scope.newgamer.level = "";
+      $scope.newgamer.bounty = "";
 
-
-
-$scope.gamers = [
-  {
-    name:"Victor",
-    level: "Pro",
-    bounty: 10000000000,
-    available: true,
-    thumb: "img/mage.png"
-
-  },
-  {
-    name:"Chris",
-    level: "Noob",
-    bounty: 2,
-    available: true,
-    thumb: "img/mush.png"
-  },
-  {
-    name:"Nhu",
-    level: "Expert",
-    bounty: 30000,
-    available: true,
-    thumb:"img/thief.png"
-  },
-  {
-    name:"Henry",
-    level: "Noob",
-    bounty: 1,
-    available: false,
-    thumb:"img/pig.png"
+    }
   }
 
-];
+
+  $scope.gamers = GamerService.getGamersList();
 
 }]);
