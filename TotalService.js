28665523abd46344a0254f576516cf7cc59d946b
TRAN.VICTOR.CHRISTOPHER.1541469393 <victor.c.tran2.ctr@mail.mil>
2018-02-12 14:52:42 -0500

    added specs

--- TotalService.js
+++ TotalService.js
@@ -1,5 +1,8 @@
 myGamerApp.service('TotalService', function() {
 
+
+
+
   this.getTotalBounty = function (gamers){
       var total = 0;
       for(var i = 0; i < gamers.length; i++){
@@ -12,12 +15,26 @@ myGamerApp.service('TotalService', function() {
 
 
 
-
       this.getTotalCount = function (gamers){
 
 
           return gamers.length;
 
           }
+
+
+      this.getGamerNames = function (gamers) {
+
+        var newArray = []
+
+        for(var i = 0; i < gamers.length; i++){
+          newArray.push(gamers[i].name);
+
+        }
+
+        return newArray;
+
+        }
+
 
 });
--- TotalService.spec.js
+++ TotalService.spec.js
@@ -8,13 +8,97 @@ describe('Total Service', function() {
       TotalService = _TotalService_;
     }));
 
-    
+
 
     it('testing if the getTotalCount function will return the length of gamers' , function() {
-      console.log(TotalService.getTotalCount.length)
-      expect(TotalService.getTotalCount.length).toEqual(1);
+
+      var testArray = [1,2];
+
+
+
+      expect(TotalService.getTotalCount(testArray)).toEqual(2);
     });
 
+
+    it('testing if the getTotalCount function will return the total count for bounty' , function() {
+
+      var testArray = [
+        {
+          name:"Victor",
+          level: "Pro",
+          bounty: 1,
+          available: true,
+          thumb: "img/mage.png"
+
+        },
+        {
+          name:"Chris",
+          level: "Noob",
+          bounty: 2,
+          available: true,
+          thumb: "img/mush.png"
+        },
+        {
+          name:"Nhu",
+          level: "Expert",
+          bounty: 3,
+          available: true,
+          thumb:"img/thief.png"
+        },
+        {
+          name:"Henry",
+          level: "Noob",
+          bounty: 1,
+          available: false,
+          thumb:"img/pig.png"
+        }
+      ];
+
+     expect(TotalService.getTotalBounty(testArray)).toEqual(7);
+    });
+
+
+        it('testing if the function will retrieve the strings of the name property ' , function() {
+
+          var testArray =  [
+            {
+              name:"Victor",
+              level: "Pro",
+              bounty: 10000000000,
+              available: true,
+              thumb: "img/mage.png"
+
+            },
+            {
+              name:"Chris",
+              level: "Noob",
+              bounty: 2,
+              available: true,
+              thumb: "img/mush.png"
+            },
+            {
+              name:"Nhu",
+              level: "Expert",
+              bounty: 30000,
+              available: true,
+              thumb:"img/thief.png"
+            },
+            {
+              name:"Henry",
+              level: "Noob",
+              bounty: 1,
+              available: false,
+              thumb:"img/pig.png"
+            }
+          ];
+
+
+          expect(TotalService.getGamerNames(testArray)[0]).toBe("Victor");
+          expect(TotalService.getGamerNames(testArray)[1]).toBe("Chris");
+          expect(TotalService.getGamerNames(testArray).length).toBe(4);
+
+
+        });
 
 
   });
